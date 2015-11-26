0ed2f33a341894f057751a15ed6b3208c14357c8
butlerd <userobject@uniserve.com>
2015-11-26 10:06:19 -0700

    Bong

--- snake.cpp
+++ snake.cpp
@@ -6,7 +6,7 @@
 
 
 enum Direction {UP,RIGHT,DOWN,LEFT};
-enum Event {MOVEDUP,MOVEDRIGHT,MOVEDDOWN,MOVEDLEFT,MOVEDHIGHER,MOVEDLOWER};
+enum Event {MOVEDUP,MOVEDRIGHT,MOVEDDOWN,MOVEDLEFT,MOVEDINTO,MOVEDOUTOF};
 const int snakeSpeed = 1;
 const int snakeWidth = 1;
 
@@ -89,9 +89,10 @@ class Snake{
     uint8_t y;
     uint16_t colour;
     Direction dir;
+    int layer;
     boolean finishedDrawing;
     public:
-    SnakeComponent(uint8_t startX,uint8_t startY,Direction startDir,uint16_t col){
+    SnakeComponent(uint8_t startX,uint8_t startY,Direction startDir,uint16_t col,int layer) : layer(layer){
       x = startX;
       y = startY;
       dir = startDir;
@@ -108,7 +109,9 @@ class Snake{
       draw();
     }
     void draw(){
-       tft.drawPixel(x,y,colour);
+       if(layer == 0){
+         tft.drawPixel(x,y,colour);
+       }
     }
     void setDirection(Direction newDirection){
       dir = newDirection;  
@@ -122,6 +125,15 @@ class Snake{
     uint8_t getY(){
       return y;  
     }
+    void setLayer(int newLayer){
+      layer = newLayer;  
+    }
+    void setX(uint8_t newX){
+      x = newX;
+    }
+    void setY(uint8_t newY){
+      y = newY;  
+    }
     boolean isFinishedDrawing(){
       return finishedDrawing;
     }
@@ -133,24 +145,24 @@ class Snake{
   snakeEvent eventQueue[128];
   uint8_t queueIndex;
   int pendingLength;
+  int length;
   int layer;
   SnakeComponent* headComp;
   SnakeComponent* tailComp;
   boolean dead;
   public:
   Snake(uint8_t startX, uint8_t startY,Direction startDir, uint16_t col, int startingLength) :
-    headComp(new SnakeComponent(startX,startY,startDir,col)) , 
-    tailComp(new SnakeComponent(startX,startY,startDir,0x0)) ,
+    layer(0),
+    headComp(new SnakeComponent(startX,startY,startDir,col,layer)) , 
+    tailComp(new SnakeComponent(startX,startY,startDir,0x0,layer)) ,
     pendingLength(startingLength),
-    layer(1),
+    length(startingLength),
     queueIndex(0),
     dead(false)
     {
     }
   void update(){
-    if(!headComp->isFinishedDrawing()){
-      headComp->update();
-    }
+    headComp->update();
     if(pendingLength != 0){
         pendingLength--;
     }
@@ -159,21 +171,11 @@ class Snake{
       Serial.println(eventQueue[0].y);
       Serial.println(eventQueue[0].e);
       if(tailComp->getX() == eventQueue[0].x && tailComp->getY() == eventQueue[0].y){
-        if(eventQueue[0].e == MOVEDHIGHER || eventQueue[0].e == MOVEDLOWER){
-          tailComp->setFinishedDrawing(true);
-          memmove(&eventQueue[0], &eventQueue[1], sizeof(eventQueue) - sizeof(*eventQueue));
-          queueIndex--;
-          tailComp->draw();
-        }
-        else{
           tailComp->setDirection((Direction)(eventQueue[0].e));
           memmove(&eventQueue[0], &eventQueue[1], sizeof(eventQueue) - sizeof(*eventQueue));
           queueIndex--;
-        }
       }
-      if(!tailComp->isFinishedDrawing()){
-        tailComp->update();  
-      }
+      tailComp->update();  
     }
   }
   void setDirection(Direction newDirection){
@@ -183,7 +185,7 @@ class Snake{
           temp.x = headComp->getX();
           temp.y = headComp->getY();
           temp.e = (Event) newDirection;
-          eventQueue[queueIndex++] = temp; 
+          addEvent(temp); 
       }   
   }
   uint8_t getX(){
@@ -191,6 +193,12 @@ class Snake{
   }
   uint8_t getY(){
     return headComp->getY();
+  }
+  void setX(uint8_t x){
+    headComp->setX(x);
+  }
+  void setY(uint8_t y){
+    headComp->setY(y);  
   }
   uint8_t getTailX(){
     return tailComp->getX();
@@ -223,6 +231,9 @@ class Snake{
   snakeEvent getEvent(int index){
     return eventQueue[index];
   }
+  void addEvent(struct snakeEvent e){
+    eventQueue[queueIndex++] = e;
+  }
   boolean isHeadDrawing(){
     return headComp->isFinishedDrawing();
   }
@@ -235,10 +246,15 @@ class Snake{
   void setTailDrawing(boolean b){
     tailComp->setFinishedDrawing(b);
   }
+  void writeSnakeToSerial(int snakeNum,HardwareSerial &s){
+    s.write(snakeNum);
+    s.write(getX());
+    s.write(getY());
+    s.write(getDirection());
+  }
 };
 
 const int fps = 30;
-
 
 //Receive changes in direction from the clients
 //Send to clients if stuff has to be drawn on their screen 
@@ -247,19 +263,39 @@ const int fps = 30;
 class GameManager{
   private:
     Snake* s[3];
+    uint8_t layer1[16][160];
+    uint8_t layer2[16][160];
     JoystickListener* js;
-    void parseClientPacket(uint8_t packet, Snake* s){
+    void parseClientPacket(HardwareSerial &serial, Snake* s, int snakeNum){
+      uint8_t packet = serial.read();
       if(packet > 3){
+        int newlayer;
         if(packet == 4){
-          s->setLayer( s->getLayer() + 1 %3);
-        }else if(packet ==5){
-          s->setLayer( s->getLayer() - 1 %3);
-          
+          newlayer = serial.read();
+          s->setLayer(newlayer);
+        switch(newlayer){
+          case 0:
+            snakeEvent e;
+            e.x = s->getX();
+            e.y = s->getY();
+            e.e = MOVEDINTO;
+            s->addEvent(e);
+            break;
+          case 1:
+            s->writeSnakeToSerial(snakeNum, Serial2);            
+            break;
         }
+      }
       }
       else{
         s->setDirection(packet);
       }
+    }
+    boolean getLayerTile(uint8_t layer[16][160], uint8_t x, uint8_t y){
+      return layer[x/8][y]&(1>>x%8);
+    }
+    void setLayerTile(uint8_t layer[16][160], uint8_t x, uint8_t y , boolean value){
+      layer[x/8][y] = (!value ^ layer[x/8][y]) & (1>>x%8);
     }
   public:    
     GameManager() : js (new JoystickListener(VERT,HOR,SEL,450)){
@@ -267,59 +303,6 @@ class GameManager{
       s[0] = new Snake(20,20,DOWN,0xFF00,20);
       s[1] = new Snake(20,100,RIGHT,0x0FF0,20);
       s[2] = new Snake(100,108,UP,0x00FF,20);
-    }
-    void detectCollision(){
-      bool isVert;
-      int index;
-      uint8_t pos,align1; //properties of the snake in focus
-      uint8_t A,B,align2; //properties of another snake
-      
-      for(int i = 0; i < 3; i++){
-        bool isVert = (s[i]->getDirection() % 2 == 0);
-        pos = isVert ? s[i]->getX() : s[i]->getY();
-        align1 = isVert ? s[i]->getY() : s[i]->getX();
-
-        for(int j = 0; j < 3; j++){
-          if(s[j]->isDead()){
-            Serial.print("Snake ");Serial.print(j);Serial.println(" is dead");
-            continue;
-          }
-          
-          int index = s[j]->getQIndex();
-          
-          if(i!=j && index == 0 && isVert!=s[j]->getDirection() % 2){
-            A = isVert ? s[j]->getTailX() : s[j]->getTailY();
-            B = isVert ? s[j]->getX() : s[j]->getY();
-            align2 = isVert ? s[j]->getY() :s[j]->getX();
-            if((align1 == align2) &&
-                  ((pos >= A && pos <= B) || (pos <= A && pos >= B))){
-              s[i]->kill();
-              break;
-            }
-          }
-          
-          //you want to check only perpendicular segments
-          index -= (isVert != s[j]->getDirection() % 2) ? 1 : 0;
-          if(i == j)index-=2; //only test edge cases for different snakes
-          
-          while(index > 0 || (index == 0 && i!=j)){
-            int Ax, Ay, Bx, By; //the positions of the top/bottom of the line;
-            Ax = (index > s[j]->getQIndex()-2) ? s[j]->getX() : s[j]->getEvent(index).x;
-            Ay = (index > s[j]->getQIndex()-2) ? s[j]->getY() : s[j]->getEvent(index).y;
-            Bx = (index == 0) ? s[j]->getTailX() : s[j]->getEvent(index-1).x;
-            By = (index == 0) ? s[j]->getTailY() : s[j]->getEvent(index-1).y;
-            
-            align2 = isVert ? Ay : Ax;
-            
-            if(align1 == align2){
-              uint8_t A = isVert ? Ax : Ay;
-              uint8_t B = isVert ? Bx : By;
-              if((pos >= A && pos <= B) || (pos <= A && pos >= B)) s[i]->kill();
-            }
-            index-=2;
-          }
-        }
-      }
     }
     void run(){
       uint32_t time = millis();
@@ -329,9 +312,14 @@ class GameManager{
           for(int i = 0; i < 3; i++){
             if(!s[i]->isDead()){
               s[i]->update();
+              if(getLayerTile((s[i]->getLayer())? layer1 : layer2, s[i]->getX(), s[i]->getY()){
+                s[i].kill();
+              }
+              else{
+                 getLayerTile((s[i]->getLayer())? layer1 : layer2, s[i]->getX(), s[i]->getY(), true);  
+              }
             }
           }
-          detectCollision();
           if(js->isPushed()){
             int deltaH = js->getHorizontal() - js->getHorizontalBaseline();
             int deltaV = js->getVertical() - js->getVerticalBaseline();
@@ -343,10 +331,10 @@ class GameManager{
             }
           }
           if(Serial2.available()){
-            parseClientPacket(Serial2.read(),s[1]);
+            parseClientPacket(Serial2,s[1], 1);
           }
           if(Serial3.available()){
-            parseClientPacket(Serial3.read(),s[2]);
+            parseClientPacket(Serial3,s[2] ,2);
           }
         }
       }  
