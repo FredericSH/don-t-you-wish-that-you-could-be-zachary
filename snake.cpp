13da68a3bb6564c47473287aa4ab7931b9e5a299
zGoldthorpe <zgoldtho@ualberta.ca>
2015-12-01 09:08:51 -0700

    This works but you'll have to redo all that you did that wasn't related to collisions...i.e I'm using the version prior to you using a circular queue

--- snake.cpp
+++ snake.cpp
@@ -21,6 +21,12 @@ const int VERT = 0;
 const int HOR = 1;
 const int SEL = 9;
 
+struct snakeEvent{
+      uint8_t x;
+      uint8_t y;
+      Event e;
+  };
+
 class JoystickListener{
   private:
     int horizontalPin;
@@ -91,8 +97,12 @@ class Snake{
       colour = col;
     }  
     void update(){
-      x -= (dir%2 == 1) ? dir - 2 : 0;  
+      x -= (dir%2 == 1) ? dir - 2 : 0;
+      if(x == 255) x = 127;
+      if(x == 128) x = 0; 
       y += (dir%2 == 0) ? dir - 1 : 0;
+      if(y == 255) y = 159;
+      if(y == 160) y = 0;
       tft.drawPixel(x,y,colour);
     }
     void setDirection(Direction newDirection){
@@ -108,14 +118,8 @@ class Snake{
       return y;  
     }
   };
-  struct snakeEvent{
-      uint8_t x;
-      uint8_t y;
-      Event e;
-  };
   snakeEvent eventQueue[128];
   uint8_t queueIndex;
-  int activeEventIndex;
   int pendingLength;
   int layer;
   SnakeComponent* headComp;
@@ -128,7 +132,6 @@ class Snake{
     pendingLength(startingLength),
     layer(1),
     queueIndex(0),
-    activeEventIndex(0),
     dead(false)
     {
     }
@@ -138,54 +141,145 @@ class Snake{
         pendingLength--;
     }
     else{
-      if(tailComp->getX() == eventQueue[activeEventIndex].x && tailComp->getY() == eventQueue[activeEventIndex].y){
-         tailComp->setDirection((Direction)(eventQueue[activeEventIndex++].e));
-         if(activeEventIndex == 128) activeEventIndex = 0; 
+      Serial.println(eventQueue[0].x);
+      Serial.println(eventQueue[0].y);
+      Serial.println(eventQueue[0].e);
+      if(tailComp->getX() == eventQueue[0].x && tailComp->getY() == eventQueue[0].y){
+        tailComp->setDirection((Direction)(eventQueue[0].e));
+        memmove(&eventQueue[0], &eventQueue[1], sizeof(eventQueue) - sizeof(*eventQueue));
+        queueIndex--;
       }
       tailComp->update();  
     }
   }
   void setDirection(Direction newDirection){
-      if(abs(newDirection - headComp->getDirection()) != 2){
+      if(newDirection%2 != headComp->getDirection()%2){
           headComp->setDirection(newDirection);
           snakeEvent temp;
           temp.x = headComp->getX();
           temp.y = headComp->getY();
           temp.e = (Event) newDirection;
           eventQueue[queueIndex++] = temp; 
-          if(queueIndex == 128)queueIndex = 0;
       }   
+  }
+  uint8_t getX(){
+    return headComp->getX();
+  }
+  uint8_t getY(){
+    return headComp->getY();
+  }
+  uint8_t getTailX(){
+    return tailComp->getX();
+  }
+  uint8_t getTailY(){
+    return tailComp->getY();
+  }
+  Direction getDirection(){
+    return headComp->getDirection();
   }
   void setDirection(int d){
     setDirection((d == 0) ? UP : (d == 1) ? RIGHT : (d == 2) ? DOWN : LEFT);  
   }
+  void kill(){
+    dead = true;
+  }
   boolean isDead(){
       return dead;  
   }
+  void setLayer(int newLayer){
+    layer = newLayer;
+  }
+  int getLayer(){
+    return layer;
+  }
+  uint8_t getQIndex(){
+    return queueIndex;
+  }
+  snakeEvent getEvent(int index){
+    return eventQueue[index];
+  }
 };
 
-const int fps = 60;
+const int fps = 30;
+
+
+//Receive changes in direction from the clients
+//Send to clients if stuff has to be drawn on their screen 
+//Changes in direction to snakes on clients screen
 
 class GameManager{
   private:
     Snake* s[3];
     JoystickListener* js;
+    void parseClientPacket(uint8_t packet, Snake* s){
+      if(packet > 3){
+        if(packet == 4){
+          s->setLayer( s->getLayer() + 1 %3);
+          
+        }else if(packet ==5){
+          s->setLayer( s->getLayer() - 1 %3);
+          
+        }
+      }
+      else{
+        s->setDirection(packet);
+      }
+    }
   public:    
-    GameManager() :js(new JoystickListener(VERT,HOR,SEL,450)){
+    GameManager() : js (new JoystickListener(VERT,HOR,SEL,450)){
       tft.fillScreen(0);
       s[0] = new Snake(20,20,DOWN,0xFF00,20);
       s[1] = new Snake(20,100,RIGHT,0x0FF0,20);
-      s[2] = new Snake(100,108,UP,0x00FF,20); 
+      s[2] = new Snake(100,108,UP,0x00FF,20);
+    }
+    bool intersects(int X, int Y, int x1, int y1, int x2, int y2){
+      if(x1 == x2){
+        return(X == x1 && Y > min(y1,y2) && Y < max(y1,y2));
+      }else{
+        return(Y == y1 && X > min(x1,x2) && X < max(x1,x2));
+      }
+    }
+    void detectCollision(){
+      int index;
+      for(int i = 0; i < 3; i++){
+        if(s[i]->isDead())continue;
+        
+        for(int j = 0; j < 3; j++){
+          index = s[j]->getQIndex();
+          if(index == 0){
+            if(intersects(s[i]->getX(),s[i]->getY(),s[j]->getX(),s[j]->getY(),s[j]->getTailX(),s[j]->getTailY())){
+              s[i]->kill();
+              break;
+            }
+          }else{
+            if(intersects(s[i]->getX(),s[i]->getY(),s[j]->getX(),s[j]->getY(),s[j]->getEvent(index-1).x,s[j]->getEvent(index-1).y)){
+              s[i]->kill();
+              break;
+            }
+            if(intersects(s[i]->getX(),s[i]->getY(),s[j]->getEvent(0).x,s[j]->getEvent(0).y,s[j]->getTailX(),s[j]->getTailY())){
+              s[i]->kill();
+              break;
+            }
+          }
+          while(--index > 0){
+            if(intersects(s[i]->getX(),s[i]->getY(),s[j]->getEvent(index).x,s[j]->getEvent(index).y,s[j]->getEvent(index-1).x,s[j]->getEvent(index-1).y)){
+              s[i]->kill();
+              break;
+            }
+          }
+        }
+      }
     }
     void run(){
       uint32_t time = millis();
-      while(true){
+      while(!(s[0]->isDead() && s[1]->isDead() && s[2]->isDead())){
         if(millis() - time > 1000/fps){
           time = millis();
           for(int i = 0; i < 3; i++){
             if(s[i]->isDead())continue;
             s[i]->update();
           }
+          detectCollision();
           if(js->isPushed()){
             int deltaH = js->getHorizontal() - js->getHorizontalBaseline();
             int deltaV = js->getVertical() - js->getVerticalBaseline();
@@ -196,16 +290,24 @@ class GameManager{
               s[0]->setDirection((deltaV > 0) ? 2 : 0);
             }
           }
+          if(Serial2.available()){
+            parseClientPacket(Serial2.read(),s[1]);
+          }
+          if(Serial3.available()){
+            parseClientPacket(Serial3.read(),s[2]);
+          }
         }
       }  
     }
-  
 };
 int main(){
   init();
   tft.initR(INITR_REDTAB); // initialize a ST7735R chip, green tab
   Serial.begin(9600);
+  Serial2.begin(9600);
+  Serial3.begin(9600);
   GameManager* gm = new GameManager();
   gm->run();
+  Serial.end();
   return 0;
-};
+}
